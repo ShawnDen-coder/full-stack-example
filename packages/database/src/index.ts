5da7610a86951c0f9451046031a8b7960d1a8f81
ShawnDen-coder <shawndeng1109@qq.com>
2026-09-09 17:44:57 +0800

    feat: add tenant transaction context

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -3,3 +3,4 @@ export { checkDatabase } from "./health.js";
 export { defaultMigrationsFolder, migrateDatabase } from "./migrate.js";
 export { todos } from "./schema/index.js";
 export { organization } from "./schema/auth.js";
+export { withTenantTransaction, type TenantTransaction } from "./tenant.js";
